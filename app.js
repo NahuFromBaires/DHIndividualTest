304c782104892d78faeae3666034188bcdb7372f
Nahu <nahuargentina@gmail.com>
2021-01-14 20:22:50 -0300

    Servidor corriendo y git configurado

---
+++ app.js
@@ -0,0 +1,23 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+
+
+
+module.exports = app;
