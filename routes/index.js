3c6f6fd98377157508886b0f0645803c3bee1b50
Nahu <nahuargentina@gmail.com>
2021-01-14 22:51:42 -0300

    El home recibe los datos por parametros

--- routes/index.js
+++ routes/index.js
@@ -5,34 +5,35 @@ const router = express.Router();
 const db = require('../database/db/models');
 const { listenerCount } = require('process');
 
-router.get('/', function(req, res) {
-  res.render( path.join(__dirname, '../views/index') )
+router.get('/', function(req, res, next) {
+ 
+  db.sequelize.query('SELECT * FROM NOTAS')
+    .then(function(resultados){
+        
+       
+       req.body.notas = resultados[0];
+      console.log(req.body.notas);
+       
+      
+      } //cierra callback 
+      
+      ) //Cierra then
+ 
+ 
+  res.render( path.join(__dirname, '../views/index') 
+  
+  
+  )
 
 });
 
 router.get('/detail', function(req, res) {
+  console.log('Estoy en detail');
   res.render( path.join(__dirname, '../views/detail') )
 
 });
 
 
-router.get('/notas', function(req, res, next) {
-
-    db.sequelize.query('SELECT * FROM NOTAS')
-    .then(function(resultados){
-        
-       
-        for(let i = 0; i<=resultados.length;i++){
-          console.log(
-          resultados[0][i]);
-        };
-       res.send('holis')
-      
-      } //cierra callback 
-      
-      ) //Cierra then
-    
-});
 
 
 module.exports = router;
