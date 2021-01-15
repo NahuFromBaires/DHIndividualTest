d97f3637647151ede47efdc52d55634c050264ac
Nahu <nahuargentina@gmail.com>
2021-01-14 21:11:29 -0300

    Server corriendo con index consigna

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
-
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-
 const app = express();
+
+const http = require('http');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -15,9 +15,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 
+
+
+/*Inicializar puerto de escucha del servidor*/
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Servidor corriendo en el puerto 3000`);
+    console.log(`http://localhost:3000`);
+  })
 
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const path = require('path')
+const express = require('express');
+const router = express.Router();
+
+router.get('/', function(req, res) {
+  res.render( path.join(__dirname, '../views/index') )
+
 });
+
+
+
+
+
+
+
 
 module.exports = router;
